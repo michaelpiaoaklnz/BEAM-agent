872d5d8ef77b0a25756749f9b81b23f9ab480b8d
Michael PIAO <jingzhaopiao@gmail.com>
2026-05-09 23:19:10 +1200

    Claude T28 original implementation

--- src/BeamApi/Program.cs
+++ src/BeamApi/Program.cs
@@ -1,37 +1,58 @@
 using BeamApi.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BeamApi", Version = "v1" });
-});
+builder.Services
+    .AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
 
-// Register custom services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<AccountsService>();
+builder.Services.AddScoped<SuppliersService>();
 builder.Services.AddScoped<OrdersService>();
+builder.Services.AddScoped<ExpensesService>();
+builder.Services.AddScoped<OrderWorkflowService>();
+builder.Services.AddScoped<RefundService>();
+builder.Services.AddScoped<ContactService>();
+builder.Services.AddScoped<OrderCancellationService>();
+builder.Services.AddScoped<CaseService>();
+builder.Services.AddScoped<UserDeactivationService>();
+builder.Services.AddScoped<TicketClosureService>();
 builder.Services.AddScoped<AuditService>();
+builder.Services.AddScoped<SearchService>();
+builder.Services.AddScoped<DiscountsService>();
+builder.Services.AddScoped<LeaveService>();
 builder.Services.AddScoped<InvoicesService>();
+builder.Services.AddScoped<TicketStateService>();
+builder.Services.AddScoped<EmployeeProfilesService>();
+builder.Services.AddScoped<ProjectsService>();
+builder.Services.AddScoped<ResourcesService>();
+builder.Services.AddScoped<DocumentsService>();
+builder.Services.AddScoped<ProductPricesService>();
 builder.Services.AddScoped<CustomersService>();
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
